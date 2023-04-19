23f71d7cb17543fef734bb5d97ad116d40cd2acc
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2023-04-19 12:05:30 +0500

    http://127.0.0.1:8000/example/

--- new_preject/new_preject/urls.py
+++ new_preject/new_preject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from example.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('example/',index),
 ]
